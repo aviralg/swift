4bf3efc28342a12707b01be7f78b88b4a8d08eed
Artem Chikin <achikin@apple.com>
2025-09-30 16:36:09 -0700

    [Performance Hints] Add simple check for returning of values of array and dictionary type
    This check will run on each type-checked primary input of the current compilation and emit a warning diagnostic for all discovered occurences of this code pattern when the performance hint diagnostic is enabled

---
+++ test/PerformanceHints/array-ret.swift
@@ -0,0 +1,29 @@
+// RUN: %empty-directory(%t)
+
+// Ensure ignored by-default
+// RUN: %target-swift-frontend -typecheck %s -diagnostic-style llvm -verify
+
+// Ensure enabled with '-Wwarning'
+// RUN: %target-swift-frontend -typecheck %s -diagnostic-style llvm -Wwarning PerformanceHints &> %t/output_warn.txt
+// RUN: cat %t/output_warn.txt | %FileCheck %s -check-prefix CHECK-WARN
+
+// Ensure escalated with '-Werror'
+// RUN: not %target-swift-frontend -typecheck %s -diagnostic-style llvm -Werror PerformanceHints &> %t/output_err.txt
+// RUN: cat %t/output_err.txt | %FileCheck %s -check-prefix CHECK-ERR
+
+// CHECK-ERR: error: Performance: 'foo()' returns an array, leading to implicit copies. Consider using an 'inout' parameter instead. [#PerformanceHints]
+// CHECK-ERR: error: Performance: closure returns an array, leading to implicit copies. Consider using an 'inout' parameter instead. [#PerformanceHints]
+
+// CHECK-WARN: warning: Performance: 'foo()' returns an array, leading to implicit copies. Consider using an 'inout' parameter instead. [#PerformanceHints]
+// CHECK-WARN: warning: Performance: closure returns an array, leading to implicit copies. Consider using an 'inout' parameter instead. [#PerformanceHints]
+
+func foo() -> [Int] {
+    return [1,2,3,4,5,6]
+}
+
+func bar() -> Int {
+    let myCoolArray = { () -> [Int] in
+        return [1, 2]
+    }
+    return myCoolArray().count
+}
